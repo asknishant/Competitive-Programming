4abdc572c46344209977ac72726a0c2a8aabe97e
Nishant Kumar <51340544+asknishant@users.noreply.github.com>
2020-06-10 23:12:07 +0530

    This is more specific approach
    Storing the least number of coins everytime inside min_coin.

---
+++ Recursion/min_number_of_coins.cpp
@@ -0,0 +1,28 @@
+//https://medium.com/@baeddavid/taking-the-first-recursive-leap-of-faith-ea5cd9582d94
+//For reference.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int makingChange(int arr[] , int c) {
+
+  if(c == 0)  return 0;
+  int min_coin = INFINITY;
+  int curr_min_coins = 0;
+  for(int i=0;i<3;i++) {
+    if(c-arr[i] >=0)
+       curr_min_coins = makingChange(arr,c-arr[i]);    ///Each call leads to looping from 0 to 2.subtracting all possible cases till c-arr[i]>=0.
+      if(curr_min_coins < min_coin)
+         min_coin = curr_min_coins;
+  }
+  return min_coin+1;
+}
+int main(void) {
+
+  int arr[] = {1,2,5};
+  int c;  cin >> c;
+
+  cout << "Number of change coins are" << makingChange(arr,c);
+  return 0;
+}
