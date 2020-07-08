dc0e1a3168fd8062b090749824948c790bd71795
Nishant Kumar <51340544+asknishant@users.noreply.github.com>
2020-07-08 20:31:19 +0530

    Kadane's Algorithm

---
+++ Sorting/kadanes_algo.cpp
@@ -0,0 +1,22 @@
+//Kadane's algorithm
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int arr[100];
+  int n;  cin >> n;
+  for(int i=0;i<n;i++)
+    cin >> arr[i];
+  
+  int cs=0;
+  int ms=0;
+  for(int i=0;i<n;i++) {
+   cs += arr[i];
+   if(cs < 0) {
+     cs = 0;
+   }
+   ms = max(cs,ms);
+  }
+  cout << ms;
+}
