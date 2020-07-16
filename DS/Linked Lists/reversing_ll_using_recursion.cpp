295fca98c550ad9fd43268e27bad330b9b9d6ce4
Nishant Kumar <51340544+asknishant@users.noreply.github.com>
2020-07-16 10:57:28 +0530

    Create reversing_ll_using_recursion.cpp

---
+++ DS/Linked Lists/reversing_ll_using_recursion.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+class node {
+  public :
+    int data;
+    node* next;
+  
+  node(int data) {
+    this->data = data;
+  }
+};
+
+void insertAtEnd(node* &head, int data) {
+  node* n = new node(data);
+  node* temp = head;
+  n->next = NULL;
+  while(temp->next != NULL) {
+    temp = temp->next;
+  }
+  temp->next = n;
+}
+
+void insertAtHead(node* &head, int data) {
+  node* n = new node(data);
+  n->next = head;
+  head = n;
+}
+
+void insertAtMiddle(node* &head, int data, int pos) {
+  node* n = new node(data);
+  node* temp = head;
+  int jumps = 0;
+  while(jumps < pos-1) {
+    temp = temp->next;
+    jumps++;
+  }
+  n->next = temp->next;
+  temp->next = n;
+
+}
+
+void print(node* &head) {
+  node* temp = head;
+  while(temp != NULL) {
+    cout << temp->data <<"-->";
+    temp = temp->next;
+  }
+}
+
+void reverseLL(node* &head) {
+  node* curr = head;
+  node* prev = NULL;
+  node* n;
+
+  while(curr != NULL) {
+    n = curr->next;
+    curr->next = prev;
+    prev = curr;
+    curr = n; 
+  }
+}
+
+node* revRec(node* head) {
+  //Passing head by value.
+  //Base Case
+  if(head == NULL || head->next == NULL) {
+    return head;
+  }
+  node* smallHead = revRec(head->next);
+  node* curr = head;
+  curr->next->next = curr;
+  curr->next = NULL;
+  return smallHead;
+}
+
+int main() {
+  node* head = NULL;
+  insertAtHead(head, 4);
+  insertAtHead(head, 6);
+  insertAtHead(head, 7);
+  insertAtEnd(head, 9);
+  insertAtMiddle(head, 1,3);
+  print(head);
+  cout << endl;
+  head = revRec(head);
+  print(head);
+  return 0;
+}
