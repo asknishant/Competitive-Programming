20ac14c82abee260f9f7cff6852e685c03c55943
Nishant Kumar <51340544+asknishant@users.noreply.github.com>
2020-08-06 12:37:02 +0530

    Create deletion_bst.cpp

---
+++ DS/Trees/BST/deletion_bst.cpp
@@ -0,0 +1,151 @@
+//  try with 5 3 7 1 6 8 -1
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class node {
+  public:
+  int data;
+  node* left;
+  node* right;
+
+  node(int data) {
+    this->data = data;
+    left = NULL;
+    right = NULL;
+  }
+};
+
+
+//Accepts the old root node and data and returns a new node.
+node* insertInBST(int d,node* root) {
+  if(root == NULL) {
+    return new node(d);
+  }
+  if(d>=root->data) {
+    root->right = insertInBST(d, root->right);
+  }
+  else if(d < root->data){
+    root->left = insertInBST(d, root->left);
+  }
+  return root;
+}
+
+node* build() {
+  int d;
+  cin >> d;
+  node* root = NULL;
+  while(d!=-1){
+    root = insertInBST(d, root);
+    cin >> d;
+  }
+  return root;
+}
+
+//level order
+void bfs(node* root) {
+  queue<node*> q;
+  q.push(root);
+  q.push(NULL);
+  while(!q.empty()) {
+    node* f = q.front();
+    if(f == NULL) {
+      cout << endl;
+      q.pop();
+      if(!q.empty()) {
+        q.push(NULL);
+      }
+    }
+    else {
+      cout << f->data << " ";
+      q.pop();
+      if(f->left) {
+        q.push(f->left);
+      }
+      if(f->right) {
+        q.push(f->right);
+      }
+    }
+  }
+}
+
+//Inorder print
+void inOrder (node* root) {
+  if( root == NULL) {
+    return;
+  }
+ 
+  inOrder(root->left);
+   cout << root->data;
+  inOrder(root->right);
+}
+
+bool search(node* root, int data) {
+  if(root == NULL) {
+    return false;
+  }
+  if(root->data == data) {
+    return true;
+  }
+  if(root->data <= data) {
+    search(root->right, data);
+  } else {
+    search(root->left , data);
+  }
+}
+
+node* deleteInBST(node* root, int data) {
+  if(root == NULL) {
+    return NULL;
+  } else if(data < root->data) {
+    root->left = deleteInBST(root->left, data);
+    return root;
+  } else if(data == root->data) {
+    //1.ifNode with zero children
+    if(root->left == NULL and root->right == NULL) {
+      delete root;
+      return NULL;
+    }
+    //2. Only one child
+    if(root->left != NULL and root->right == NULL) {
+      node* temp = root->left;
+      delete root;
+      return temp;
+    }
+    if(root->right != NULL and root->left == NULL) {
+      node* temp = root->right;
+      delete root;
+      return temp;
+    }
+
+    //3.Both the children are present.
+    node* replace = root->right;
+    //Finding inorder successor.
+    while(replace->left != NULL) {
+      replace = replace->left;
+    }
+
+    root->data = replace->data;
+    root->right = deleteInBST(root->right, replace->data);
+    return root;
+    
+  } else {
+    root->right = deleteInBST(root->right, data);
+    return root;
+  }
+}
+int main() {
+  node* root = build();
+  inOrder(root);
+  cout << endl;
+
+
+  int s;  cin >> s;
+
+  root = deleteInBST(root, s);
+  inOrder(root);
+  cout << endl;
+  bfs(root);
+  return 0;
+}
