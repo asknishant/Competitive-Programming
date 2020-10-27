eaf68adf15a9033f801e2801227fe358beaba6ce
Nishant Kumar <51340544+asknishant@users.noreply.github.com>
2020-10-27 16:30:48 +0530

    Create keypad_problem.cpp

---
+++ Recursion/keypad_problem.cpp
@@ -0,0 +1,33 @@
+//0-1 Knapsack problem.
+#include <iostream>
+using namespace std;
+
+char keypad[][10] = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+void generate_strings(char *in, char *out,int i, int j) {
+    if(in[i] == '\0') {
+        out[j] = '\0';
+        cout << out <<endl;
+        return;
+    }
+    int digit = in[i] - '0'; 
+    if(digit == 0 or digit == 1) {
+        //Ignore 1 and 0 and move to next digit.
+        generate_strings(in,out,i+1,j);
+    }
+    for(int k=0;keypad[digit][k]!='\0';k++){
+        out[j] = keypad[digit][k];
+        //Fill the remaining part.
+        generate_strings(in,out,i+1,j+1);
+    }
+    return;
+}
+
+int main() {   
+
+   char in[100];
+   char out[100];
+   cin >> in;
+
+    generate_strings(in,out,0,0);
+    return 0;
+}
