59af76cf4f5b7030256c18362c353e0ac8938612
Nishant Kumar <51340544+asknishant@users.noreply.github.com>
2020-12-26 16:26:22 +0530

    Update top_k_freqent_elements.cpp

--- DS/Heaps/top_k_freqent_elements.cpp
+++ DS/Heaps/top_k_freqent_elements.cpp
@@ -35,4 +35,34 @@ public:
         }
         return v;
     }
+};  //O(N*K)
+
+
+//Solution 2 O(NlogK)
+
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        
+        unordered_map<int,int> mp;
+        for(auto const x : nums) {
+            mp[x]++;
+        }
+        
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        for(auto x : mp) {
+            pq.push({x.second,x.first});
+            while(pq.size() >k) {
+                pq.pop();
+            }
+        }
+        
+        vector<int> ans;
+        for(int i=0;i<k;i++) {
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
+    }
 };
